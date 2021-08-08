abdfc9d6229e88aa2da37d72f98e82520c25da4e
jamilligioielli <jamilligioielli@outlook.com>
2021-08-08 03:40:16 -0300

    Versão Mobile finalizada

--- projeto/PB2/js/script.js
+++ projeto/PB2/js/script.js
@@ -1,5 +1,11 @@
-function switchMode() {
-    //Futura função de troca de modos
+/* Botão voltar para o topo */
+const backToTopButton = document.querySelector('.back-to-top')
+function backToTop() {
+	if (window.scrollY >= 400) {
+		backToTopButton.classList.add('show')
+	} else {
+		backToTopButton.classList.remove('show')
+	}
 }
 
 /*  abre e fecha o menu quando clicar no icone: hamburguer e x */
@@ -7,16 +13,35 @@ const nav = document.querySelector('#header nav')
 const toggle = document.querySelectorAll('nav .toggle')
 
 for (const element of toggle) {
-  element.addEventListener('click', function () {
-    nav.classList.toggle('show')
-  })
+	element.addEventListener('click', function () {
+		nav.classList.toggle('show')
+	})
 }
 
 /* quando clicar em um item do menu, esconder o menu */
 const links = document.querySelectorAll('nav ul li a')
 
 for (const link of links) {
-  link.addEventListener('click', function () {
-    nav.classList.remove('show')
-  })
+	link.addEventListener('click', function () {
+		nav.classList.remove('show')
+	})
 }
+
+/* Testimonials carousel slider swiper */
+const swiper = new Swiper('.swiper-container', {
+	slidesPerView: 1,
+	navigation: {
+		nextEl: '.swiper-button-next',
+		prevEl: '.swiper-button-prev'
+	},
+	pagination: {
+		el: '.swiper-pagination'
+	},
+	mousewheel: true,
+	keyboard: true
+})
+
+/* When Scroll */
+window.addEventListener('scroll', function () {
+  backToTop()
+})
