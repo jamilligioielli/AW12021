99008e777f99c852a9f79d371c123c01612a14fe
jamilligioielli <jamilligioielli@outlook.com>
2021-08-10 01:27:01 -0300

    Detalhes ajustados

--- projeto/PB2/js/script.js
+++ projeto/PB2/js/script.js
@@ -26,8 +26,21 @@ for (const link of links) {
 		nav.classList.remove('show')
 	})
 }
+/* mudar o header da página quando der scroll */
+const header = document.querySelector('#header')
+const navHeight = header.offsetHeight
 
-/* Testimonials carousel slider swiper */
+function changeHeaderWhenScroll() {
+
+  if (window.scrollY >= navHeight) {
+    // scroll é maior que a altura do header
+    header.classList.add('scroll')
+  } else {
+    // menor que a altura do header
+    header.classList.remove('scroll')
+  }
+}
+/* Skills carousel slider swiper */
 const swiper = new Swiper('.swiper-container', {
 	slidesPerView: 1,
 	navigation: {
@@ -41,7 +54,61 @@ const swiper = new Swiper('.swiper-container', {
 	keyboard: true
 })
 
+/* ScrollReveal: Mostrar elementos quando der scroll na página */
+const scrollReveal = ScrollReveal({
+  origin: 'top',
+  distance: '30px',
+  duration: 600,
+  reset: true
+})
+
+scrollReveal.reveal(
+	`#home .site-subtitle, #home .site-title, #home img, #home i,
+  #home .text, #home .download-buttons,
+  #about .image, #about .text,
+  #skills .site-subtitle, #skills i,
+  #projects header, #projects .slider,
+  #contact .site-subtitle, #contact .text, #contact .form,
+  footer .brand, footer i
+  `,
+	{ interval: 50 }
+)
+
+
+
+// Menu ativo conforme a seção visivel na página //
+const sections = document.querySelectorAll('main section[id]')
+function activateMenuAtCurrentSection(){
+
+  const checkpoint = window.pageYOffset + (window.innerHeight / 8) * 4
+
+  for (const section of sections) {
+    const sectionTop = section.offsetTop
+    const sectionHeight = section.offsetHeight
+    const sectionId = section.getAttribute('id')
+
+    const checkpointStart = checkpoint >= sectionTop
+    const checkpointEnd = checkpoint <= sectionTop + sectionHeight
+    
+    if (checkpointStart && checkpointEnd) {
+      document
+        .querySelector('nav ul li a[href*=' + sectionId + ']')
+      .classList.add('active')
+    } else {
+      document
+				.querySelector('nav ul li a[href*=' + sectionId + ']')
+				.classList.remove('active')
+      
+    }
+
+  }
+
+
+}
+
 /* When Scroll */
 window.addEventListener('scroll', function () {
-  backToTop()
+  changeHeaderWhenScroll()
+	backToTop()
+	activateMenuAtCurrentSection()
 })
