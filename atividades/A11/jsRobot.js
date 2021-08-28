7d0889a9b7ed31f910c43f4d5bcdd994a10cf64d
jamilligioielli <jamilligioielli@outlook.com>
2021-08-27 23:43:32 -0300

    Js robot script

---
+++ atividades/A11/jsRobot.js
@@ -0,0 +1,9 @@
+//Level 1 - Script
+function init(robot) {
+	console.log('Robot initializing...')
+	robot.action = { type: 'move', amount: 40 }
+}
+
+function loop(robot) {
+	console.log('Robot looping...')
+}
