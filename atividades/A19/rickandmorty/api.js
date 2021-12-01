b827714857e975d8966cb6aaf3476194e36aff52
jamilligioielli <jamilligioielli@outlook.com>
2021-11-30 23:51:23 -0300

    Ajustando detalhes na Atividade 19

--- atividades/A19/rickandmorty/api.js
+++ atividades/A19/rickandmorty/api.js
@@ -9,19 +9,24 @@ fetch('https://rickandmortyapi.com/api/character', {
 			doc.innerHTML +=
 				`
         		<div class="card my-3 bg-dark border-light" style="width: 15rem;">
-				 <img class="card-img-top img-fluid" src =` +
-				results.image +
-				`  > 
+				 <img class="card-img-top img-fluid" src =` + results.image + `  > 
 					<div class="card-body text-light"> 
-					<div class='card-header text-center bg-success fw-bolder fst-italic'>` +
-				results.name +
-				`  </div> 
+						<div class='card-header text-center bg-success fw-bolder fst-italic'>` +
+							results.name +
+					`  </div> 
+						<div class="card-text text-light text-center"> 	
+					` +
+							results.status + ` - ` + results.species +
+					`
+						</div>
+						<div class="text-light text-center"> 
+						Gender: 
+							` +
+							results.gender +
+					`
+						</div>
 					</div>
-					<div class="card-text text-light text-center"> 	
-				` +
-					results.status + ` - ` + results.species +
-				`
-					</div>
+					
 				</div>
         `
 		})
